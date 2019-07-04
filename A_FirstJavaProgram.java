2b51d772d2ca3335434181b9f0501fa51471e2e6
jsw5170 <agdres1921@gmail.com>
2019-07-04 16:44:51 +0900

    Revert "영어"
    This reverts commit af11bf4b5b51c84a6e695804927b266710fe2a22.

--- A_FirstJavaProgram.java
+++ A_FirstJavaProgram.java
@@ -3,7 +3,6 @@ package ch01;
 public class A_FirstJavaProgram {
 
 	public static void main(String[] args) {
-		//영어 
 		System.out.println("Welcome Java");
 		System.out.println("First Java Program");
 
