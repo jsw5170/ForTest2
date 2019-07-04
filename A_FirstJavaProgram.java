085aa087d9a6c00b9bd3e4ff49964eb8f0d77348
jsw5170 <agdres1921@gmail.com>
2019-07-04 16:42:03 +0900

    한글 출력

--- A_FirstJavaProgram.java
+++ A_FirstJavaProgram.java
@@ -4,7 +4,7 @@ public class A_FirstJavaProgram {
 
 	public static void main(String[] args) {
 		//한글 
-		System.out.println("Welcome Java");
+		System.out.println("한글Welcome Java");
 		System.out.println("First Java Program");
 
 	}
