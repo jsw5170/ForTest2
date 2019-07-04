92a363d43a4e64f50b28bdfa5e1541ed09223367
jsw5170 <agdres1921@gmail.com>
2019-07-04 16:41:12 +0900

    Revert "3+5 추가"
    This reverts commit 204efb16580b95b4cffacf93de1fe673300861a5.

--- A_FirstJavaProgram.java
+++ A_FirstJavaProgram.java
@@ -5,7 +5,7 @@ public class A_FirstJavaProgram {
 	public static void main(String[] args) {
 		System.out.println("Welcome Java");
 		System.out.println("First Java Program");
-		System.out.println(3+5);
+
 	}
 
 }
