8f5136ee37a36199f8748237e7a810c0625854bd
jsw5170 <agdres1921@gmail.com>
2019-07-04 16:55:25 +0900

    계산 추가

--- A_FirstJavaProgram.java
+++ A_FirstJavaProgram.java
@@ -3,9 +3,10 @@ package ch01;
 public class A_FirstJavaProgram {
 
 	public static void main(String[] args) {
-		//한글 
-		System.out.println("환영합니다. Java");
+		//English 
+		System.out.println("Welcome 1 Java");
 		System.out.println("First Java Program");
+		System.out.println(3+5);
 		System.out.println(3+5);
 	}
 
