09a0c46d1ffabbee0c090dd1367e1f462184d384
jsw5170 <agdres1921@gmail.com>
2019-07-04 16:40:18 +0900

    최초 작성

---
+++ A_FirstJavaProgram.java
@@ -0,0 +1,11 @@
+package ch01;
+
+public class A_FirstJavaProgram {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome Java");
+		System.out.println("First Java Program");
+
+	}
+
+}
