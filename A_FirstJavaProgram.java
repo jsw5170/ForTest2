ac0718cbaffc20a5e8204f4ce151d1591a8ea7ba
jsw5170 <agdres1921@gmail.com>
2019-07-04 16:54:10 +0900

    영어 출력

--- A_FirstJavaProgram.java
+++ A_FirstJavaProgram.java
@@ -3,8 +3,8 @@ package ch01;
 public class A_FirstJavaProgram {
 
 	public static void main(String[] args) {
-		//한글 
-		System.out.println("환영합니다. Java");
+		//English 
+		System.out.println("Welcome 1 Java");
 		System.out.println("First Java Program");
 		System.out.println(3+5);
 	}
